f94e0db7a3fcb976f7a37e040fecb485cb333ab4
StephanLex <stephan@stephan-lex.de>
2010-10-17 13:46:48 +0200

    Layout und Routing fertsch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,13 @@ class PagesController < ApplicationController
     @title = "About"
   end
 
+  def help
+    @title = "Help"
+  end
+
+  def contact
+    @title = "Help"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,13 @@ RailsTest::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  match '/signup',  :to => 'users#new'
+
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
